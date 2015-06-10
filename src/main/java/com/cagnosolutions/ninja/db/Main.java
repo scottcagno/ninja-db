84754bca9cca5cb72cbf2f884d7e8965895675c6
scottcagno <scottiecagno@gmail.com>
2015-06-09 23:02:22 -0400

    first commit

---
+++ src/main/java/com/cagnosolutions/ninja/db/Main.java
@@ -0,0 +1,12 @@
+package com.cagnosolutions.ninja.db;
+
+/**
+ * Created by Scott Cagno.
+ * Copyright Cagno Solutions. All rights reserved.
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.printf("Hello, from NinjaDB\n");
+	}
+}
