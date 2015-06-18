a58df56e7d2799f49e474a3453a1d54c302a69b5
scottcagno <scottiecagno@gmail.com>
2015-06-18 15:31:54 -0400

    ninja-db[M0] - kiss. broke away test calls, and added export (as json) method. starting to work on the query methods.

--- src/main/java/com/cagnosolutions/ninja/Main.java
+++ src/main/java/com/cagnosolutions/ninja/Main.java
@@ -1,8 +1,5 @@
 package com.cagnosolutions.ninja;
 
-import com.cagnosolutions.ninja.database.test.MergeObjects;
-import com.cagnosolutions.ninja.database.test.Person;
-
 /**
  * Created by Scott Cagno.
  * Copyright Cagno Solutions. All rights reserved.
@@ -12,14 +9,7 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 
-		Person p1 = new Person(1, "Scott Cagno", "scottiecagno@gmail.com", false);
-		Person p2 = new Person(1, null, "scottiecagno@gmail.com", true); // active is updated, name is null
 
-		MergeObjects mergeService = new MergeObjects();
-		Person newPerson = (Person) mergeService.merge(p1, p2);
-		System.out.println(newPerson);
 
-		//Person newPerson = (Person) mergeService.MapToObject(p1m, new Person());
-		//System.out.println(newPerson);
 	}
 }
